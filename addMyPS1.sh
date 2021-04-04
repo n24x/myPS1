cddf464f3c175c810d90d02919e0deff241ca10a
Norbert <norbi44@hotmail.com>
2021-04-04 15:23:50 -0400

    added extra new line

--- addMyPS1.sh
+++ addMyPS1.sh
@@ -3,4 +3,4 @@ echo "#New PS1 Line" >> ~/.bashrc
 echo " " >> ~/.bashrc
 echo 'export PS1="\[\033[38;5;196m\]--\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;46m\]\u\[$(tput sgr0)\]\[\033[38;5;226m\]@\[$(tput sgr0)\]\[\033[38;5;14m\]\h\[$(tput sgr0)\]]\[$(tput sgr0)\]\[\033[38;5;196m\]-\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;82m\]\w\[$(tput sgr0)\]]\n\[$(tput sgr0)\]\[\033[38;5;196m\]----\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]" ' >> ~/.bashrc
 echo "colorscheme desert" > ~/.vimrc
-mostNotinstalled.sh
+./mostNotinstalled.sh
--- mostNotinstalled.sh
+++ mostNotinstalled.sh
@@ -5,6 +5,6 @@ if rpm -q most | grep "not installed" > /dev/null
        sudo yum -y update && yum install -y most
 fi
 
-
+echo # installed color in man pages
 echo 'export PAGER="/usr/bin/most -s"' >> ~/.bashrc
 source ~/.bashrc
