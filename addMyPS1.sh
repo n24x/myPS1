23e402c133ef7dcd0ef7f3f8857aea0601ad8ff9
Norbert <norbi44@hotmail.com>
2021-04-04 08:59:24 -0400

    adding .vimrc files with a theam

--- addMyPS1.sh
+++ addMyPS1.sh
@@ -1,3 +1,4 @@
 echo  >> ~/.bashrc
 echo "New PS1 Line" >> ~/.bashrc
 echo 'export PS1="\[\033[38;5;196m\]--\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;46m\]\u\[$(tput sgr0)\]\[\033[38;5;226m\]@\[$(tput sgr0)\]\[\033[38;5;14m\]\h\[$(tput sgr0)\]]\[$(tput sgr0)\]\[\033[38;5;196m\]-\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;82m\]\w\[$(tput sgr0)\]]\n\[$(tput sgr0)\]\[\033[38;5;196m\]----\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]" ' >> ~/.bashrc
+echo "colorscheme desert" > ~/.vimrc
