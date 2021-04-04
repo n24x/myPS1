50cb3eb70f66edff895e96d44e199866aac445d7
Norbert <norbi44@hotmail.com>
2021-04-04 15:29:46 -0400

    added extra new line and fixed formating

--- mostNotinstalled.sh
+++ mostNotinstalled.sh
@@ -5,6 +5,7 @@ if rpm -q most | grep "not installed" > /dev/null
        sudo yum -y update && yum install -y most
 fi
 
-echo # installed color in man pages
+echo " # installed color in man pages" >> ~/.bashrc
+echo " " >> ~/.bashrc
 echo 'export PAGER="/usr/bin/most -s"' >> ~/.bashrc
 source ~/.bashrc
