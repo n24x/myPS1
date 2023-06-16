d94f89dabd2a84c449c6108d28576d72b181c3fb
Norbert <norbertk@scinet.utoronto.ca>
2023-06-16 09:54:41 -0400

    adding my file and updating addmyps1

--- addMyPS1.sh
+++ addMyPS1.sh
@@ -1,7 +1,3 @@
-echo  >> ~/.bashrc
-echo "#New PS1 Line" >> ~/.bashrc
-echo " " >> ~/.bashrc
-echo 'export PS1="\[\033[38;5;196m\]--\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;46m\]\u\[$(tput sgr0)\]\[\033[38;5;226m\]@\[$(tput sgr0)\]\[\033[38;5;14m\]\h\[$(tput sgr0)\]]\[$(tput sgr0)\]\[\033[38;5;196m\]-\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;82m\]\w\[$(tput sgr0)\]]\n\[$(tput sgr0)\]\[\033[38;5;196m\]----\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]" ' >> ~/.bashrc
-
-cat vimrc >~/.vimrc
+cat my_settings >> ~/.bashrc
+cat vimrc >~/.vimr
 source ~/.bashrc
